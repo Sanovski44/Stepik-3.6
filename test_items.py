f5668e45a9bdf97a682470c45b2c42788fad92b5
Sanovski44 <sanovski44@gmail.com>
2021-02-11 16:14:36 +0300

    final ver.

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ import time
 
 def test_search_button_basket(browser):
     browser.get(link)
-    time.sleep(1)
-    x = browser.find_element_by_css_selector(".btn-add-to-basket")
-    assert
+    time.sleep(1) #Для визуальной проверки языка изменить на 30
+    x = len(browser.find_elements_by_css_selector(".btn-add-to-basket"))
+    assert x > 0, 'Button Basket not find'
